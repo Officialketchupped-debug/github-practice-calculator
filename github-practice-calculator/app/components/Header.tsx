aaf295a94887d92edfec1c503afa2a9d9580c221
Jamesnel Chavez <officialketchupped@gmail.com>
2025-09-29 18:22:36 +0800

    Create Header Component for Tip Calculator

---
+++ github-practice-calculator/app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="min-h-screen text-green-900 flex items-center justify-center p-4">
+            SPLIT <br/> TTER
+        </div>
+    )
+}
